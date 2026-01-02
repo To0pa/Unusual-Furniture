e57e5e55c6546fdc42a2ee5f8fc966064de32919
To0pa <toopa.fr@gmail.com>
2026-01-02 18:29:49 +0100

    added sit sounds

--- common/src/main/java/net/toopa/unusual_furniture/common/block/ISittableBlock.java
+++ common/src/main/java/net/toopa/unusual_furniture/common/block/ISittableBlock.java
@@ -1,9 +1,12 @@
 package net.toopa.unusual_furniture.common.block;
 
 import net.toopa.unusual_furniture.common.entity.SeatEntity;
+import net.toopa.unusual_furniture.common.reg.UFBlockTags;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
@@ -16,6 +19,12 @@ public interface ISittableBlock {
 			SeatEntity entity = SeatEntity.of(world, pos, dir);
 			if (world.addFreshEntity(entity)) {
 				player.startRiding(entity);
+				BlockState state = world.getBlockState(pos);
+				if (state.is(UFBlockTags.CHAIR) || state.is(UFBlockTags.STOOL) || state.is(UFBlockTags.BENCH)) {
+					world.playSound(null, pos, SoundEvents.WOOD_STEP, SoundSource.BLOCKS, 0.5F, 0.5F);
+				} else if (state.is(UFBlockTags.SOFA)) {
+					world.playSound(null, pos, SoundEvents.WOOL_STEP, SoundSource.BLOCKS, 0.5F, 0.5F);
+				}
 				return true;
 			} else {
 				entity.removeSeat();
