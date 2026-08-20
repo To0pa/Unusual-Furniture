ee4942e8ec9cacdaf8b5366ad739f589d0281f2e
macuguita <me@macuguita.com>
2026-08-20 22:45:42 +0200

    drawer changes

--- src/main/java/net/toopa/unusual_furniture/common/block/DrawerBlock.java
+++ src/main/java/net/toopa/unusual_furniture/common/block/DrawerBlock.java
@@ -1,54 +1,49 @@
 package net.toopa.unusual_furniture.common.block;
 
 import com.mojang.serialization.MapCodec;
-
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.Container;
+import net.minecraft.world.Containers;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
-
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.ChestBlock;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
+import net.minecraft.world.level.block.Mirror;
+import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.block.Rotation;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
-
 import net.minecraft.world.level.block.entity.BlockEntityType;
-
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.phys.BlockHitResult;
 import net.toopa.unusual_furniture.common.block.entity.DrawerBlockEntity;
 import net.toopa.unusual_furniture.common.reg.UFBlockEntityTypes;
 import org.jetbrains.annotations.Nullable;
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.MenuProvider;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.inventory.AbstractContainerMenu;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.context.BlockPlaceContext;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.EntityBlock;
-import net.minecraft.world.level.block.HorizontalDirectionalBlock;
-import net.minecraft.world.level.block.RenderShape;
-import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.properties.BlockStateProperties;
-import net.minecraft.world.level.block.state.properties.BooleanProperty;
-import net.minecraft.world.phys.BlockHitResult;
+public class DrawerBlock extends BaseEntityBlock {
 
-public class DrawerBlock extends HorizontalDirectionalBlock implements EntityBlock {
-
+	public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
 	private static final MapCodec<DrawerBlock> CODEC = simpleCodec(DrawerBlock::new);
 
 	public DrawerBlock(Properties properties) {
-		super(properties
-				.noOcclusion());
-		registerDefaultState(defaultBlockState()
-				.setValue(FACING, Direction.NORTH));
+		super(properties.noOcclusion());
+		this.registerDefaultState(this.defaultBlockState().setValue(FACING, Direction.NORTH));
 	}
 
 	@Override
-	protected RenderShape getRenderShape(BlockState state) {
-		return RenderShape.ENTITYBLOCK_ANIMATED;
+	protected MapCodec<? extends BaseEntityBlock> codec() {
+		return CODEC;
 	}
 
 	@Override
@@ -59,8 +54,62 @@ public class DrawerBlock extends HorizontalDirectionalBlock implements EntityBlo
 
 	@Override
 	public BlockState getStateForPlacement(BlockPlaceContext context) {
-		return this.defaultBlockState()
-				.setValue(FACING, context.getHorizontalDirection().getOpposite());
+		return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
+	}
+
+	@Override
+	protected BlockState rotate(BlockState blockState, Rotation rotation) {
+		return blockState.setValue(FACING, rotation.rotate(blockState.getValue(FACING)));
+	}
+
+	@Override
+	protected BlockState mirror(BlockState blockState, Mirror mirror) {
+		return blockState.rotate(mirror.getRotation(blockState.getValue(FACING)));
+	}
+
+	@Override
+	protected RenderShape getRenderShape(BlockState state) {
+		return RenderShape.ENTITYBLOCK_ANIMATED;
+	}
+
+	@Override
+	protected InteractionResult useWithoutItem(BlockState state, Level level, BlockPos pos, Player player, BlockHitResult hitResult) {
+		if (level.isClientSide) {
+			return InteractionResult.SUCCESS;
+		}
+
+		BlockEntity be = level.getBlockEntity(pos);
+		if (be instanceof DrawerBlockEntity drawer) {
+			player.openMenu(drawer);
+		}
+		return InteractionResult.CONSUME;
+	}
+
+	@Override
+	protected void onRemove(BlockState state, Level level, BlockPos pos, BlockState newState, boolean isMoving) {
+		if (!state.is(newState.getBlock())) {
+			BlockEntity be = level.getBlockEntity(pos);
+			if (be instanceof DrawerBlockEntity drawer) {
+				Containers.dropContents(level, pos, drawer);
+				level.updateNeighbourForOutputSignal(pos, this);
+			}
+			super.onRemove(state, level, pos, newState, isMoving);
+		}
+	}
+
+	@Override
+	protected void tick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
+		if (level.getBlockEntity(pos) instanceof DrawerBlockEntity drawer) {
+			drawer.recheckOpen();
+		}
+	}
+
+	@Override
+	@Nullable
+	public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState state, BlockEntityType<T> type) {
+		return level.isClientSide
+				? createTickerHelper(type, UFBlockEntityTypes.DRAWER_BLOCK_ENTITY, DrawerBlockEntity::drawerAnimateTick)
+				: null;
 	}
 
 	@Override
@@ -69,67 +118,8 @@ public class DrawerBlock extends HorizontalDirectionalBlock implements EntityBlo
 	}
 
 	@Override
-	public int getAnalogOutputSignal(BlockState blockState, Level world, BlockPos pos) {
-		BlockEntity tileentity = world.getBlockEntity(pos);
-		if (tileentity instanceof DrawerBlockEntity be) {
-			return AbstractContainerMenu.getRedstoneSignalFromContainer(be);
-		} else {
-			return 0;
-		}
-	}
-
-	@Override
-	protected void tick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
-		BlockEntity be = level.getBlockEntity(pos);
-
-		if (be instanceof DrawerBlockEntity drawer) {
-			drawer.recheckOpen();
-		}
-	}
-
-	@Override
-	@Nullable
-	public <T extends BlockEntity> BlockEntityTicker<T> getTicker(
-			Level level,
-			BlockState state,
-			BlockEntityType<T> type) {
-
-		return level.isClientSide
-				? (levelx, blockPos, blockState, blockEntity) ->
-				DrawerBlockEntity.drawerAnimateTick(levelx, blockPos, blockState, (DrawerBlockEntity) blockEntity)
-				: null;
-	}
-
-	@Override
-	protected boolean triggerEvent(BlockState blockState, Level level, BlockPos blockPos, int i, int j) {
-		super.triggerEvent(blockState, level, blockPos, i, j);
-		BlockEntity blockEntity = level.getBlockEntity(blockPos);
-		return blockEntity != null && blockEntity.triggerEvent(i, j);
-	}
-
-	@Override
-	protected InteractionResult useWithoutItem(BlockState state, Level level, BlockPos pos, Player player, BlockHitResult hitResult) {
-		if (!level.isClientSide) {
-			BlockEntity be = level.getBlockEntity(pos);
-			if (be instanceof DrawerBlockEntity drawer) {
-				player.openMenu(drawer);
-			}
-		}
-		return InteractionResult.sidedSuccess(level.isClientSide);
-	}
-
-	@Override
-	public MenuProvider getMenuProvider(BlockState state, Level level, BlockPos pos) {
-		BlockEntity be = level.getBlockEntity(pos);
-		if (be instanceof DrawerBlockEntity drawer) {
-			return drawer;
-		}
-		return null;
-	}
-
-	@Override
-	protected MapCodec<? extends HorizontalDirectionalBlock> codec() {
-		return CODEC;
+	public int getAnalogOutputSignal(BlockState state, Level level, BlockPos pos) {
+		return AbstractContainerMenu.getRedstoneSignalFromContainer((Container) level.getBlockEntity(pos));
 	}
 
 	@Override
