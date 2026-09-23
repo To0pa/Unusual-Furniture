cd3fcb38f627cb4b9f874b81b4b04dc2d0af006c
macuguita <me@macuguita.com>
2026-09-23 09:18:36 +0200

    update gradle stuff

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,8 +13,8 @@ pluginManagement {
 
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-    id("dev.kikugie.stonecutter") version "0.9.7"
-    id("dev.kikugie.loom-back-compat") version "0.3"
+    id("dev.kikugie.stonecutter") version "0.10-alpha.10"
+    id("dev.kikugie.loom-back-compat") version "0.4.2"
 }
 
 stonecutter {
